2c43d5af49106c4f02bd8f6f2f8ac8b297773c7e
Deece94 <Dcollar@hotmail.com.au>
2021-11-06 21:56:53 +1100

    added get by id, put and delete for household

--- controllers/households.js
+++ controllers/households.js
@@ -2,12 +2,28 @@ const householdsRouter = require('express').Router();
 const Household = require('../models/household');
 
 householdsRouter.get('/', async (request, response) => {
-    Household.find({}).then(households => {
-        response.json(households);
-    });
+    try {
+        Household.find({}).then(households => {
+            response.json(households);
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: error
+        });
+    }
+
 })
 
 householdsRouter.get('/:id', async (request, response) => {
+    try {
+        Household.findById(request.params.id).then(household => {
+            response.json(household)
+        });
+    } catch (error) {
+        return response.status(500).json({
+            error: error
+        });
+    }
 
 })
 
@@ -20,28 +36,59 @@ householdsRouter.post('/', async (request, response) => {
                 error: 'name missing'
             })
         }
-    
+
         const household = new Household({
             name: body.name,
-            date: new Date(),
+            date_created: new Date(),
         })
-    
+
         household.save().then(savedHousehold => {
             response.json(savedHousehold)
         })
 
-    } catch(error) {
-        console.log(error);
+    } catch (error) {
+        return response.status(500).json({
+            error: error
+        });
     }
-    
+
 })
 
 householdsRouter.delete('/:id', async (request, response) => {
-
+    try {
+        Household.findByIdAndRemove(request.params.id)
+            .then(result => {
+                response.status(204).end()
+            })
+            .catch(error => next(error))
+    } catch (error) {
+        return response.status(500).json({
+            error: error
+        });
+    }
 })
 
 householdsRouter.put('/:id', async (request, response) => {
+    try {
+        const body = request.body
 
+        const household = {
+            name: body.name,
+            date_created: new Date()
+        }
+
+        Household.findByIdAndUpdate(request.params.id, household, {
+                new: true
+            })
+            .then(updatedhousehold => {
+                response.json(updatedhousehold)
+            })
+            .catch(error => next(error))
+    } catch (error) {
+        return response.status(500).json({
+            error: error
+        });
+    }
 })
 
 module.exports = householdsRouter
